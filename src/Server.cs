7815b107bfee3971b33ed8413c4c86a4483bf0c4
nsw-dev <61068179+nsw-dev@users.noreply.github.com>
2023-01-15 04:22:35 -0500

    First attempt at passing stage 2

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,26 @@
 using System.Net;
+using System.Text;
 using System.Net.Sockets;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
-server.Start();
-server.AcceptSocket(); // wait for client
+IPAddress IP_ADDRESS = IPAddress.Any;
+int port = 6379;
+TcpListener server = new TcpListener(IP_ADDRESS, port);
+
+try 
+{    
+    server.Start();
+    Console.WriteLine($"Server started with IP Address : {IP_ADDRESS} and port: {port}");
+    while (true)
+    {
+        using var socket = await server.AcceptSocketAsync();
+        var pingResponse = Encoding.ASCII.GetBytes("+PONG\r\n");
+        await socket.SendAsync(pingResponse, SocketFlags.None);
+    }
+}
+finally
+{
+    server.Stop();
+}
