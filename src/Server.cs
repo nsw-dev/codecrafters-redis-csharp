399737a9b8c8645a1de11255cb3dd6abaa2025e5
nsw-dev <61068179+nsw-dev@users.noreply.github.com>
2023-01-15 04:54:04 -0500

    Third attempt at passing stage 3

--- src/Server.cs
+++ src/Server.cs
@@ -8,17 +8,18 @@ Console.WriteLine("Logs from your program will appear here!");
 IPAddress IP_ADDRESS = IPAddress.Any;
 int port = 6379;
 TcpListener server = new TcpListener(IP_ADDRESS, port);
+var pingResponse = Encoding.ASCII.GetBytes("+PONG\r\n");
 
-try 
+try
 {    
     server.Start();
     Console.WriteLine($"Server started with IP Address : {IP_ADDRESS} and port: {port}");
     using var tcpClient = server.AcceptTcpClient();
+    tcpClient.Client.Send(pingResponse, SocketFlags.None);
     using var streamReader = new StreamReader(tcpClient.GetStream());
     while (!string.IsNullOrWhiteSpace(streamReader.ReadLine()))
     {
-        // Send "PONG" to any incoming message on the socket.
-        var pingResponse = Encoding.ASCII.GetBytes("+PONG\r\n");
+        // Send "PONG" to any incoming message on the socket.        
         tcpClient.Client.Send(pingResponse, SocketFlags.None);
     }
 }
